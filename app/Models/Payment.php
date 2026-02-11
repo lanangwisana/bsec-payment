6006ede99fde2162593851e1020605debc41133f
lanangwisana <lanangwisana@gmail.com>
2026-02-11 19:42:39 +0800

    uupdate Payment: add atribut user_id in fillabble

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -7,11 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     protected $fillable = [
-        'invoice_id', 'paid_at', 'method', 'amount', 'status', 'proof_path', 'notes'
+        'invoice_id', 'user_id', 'paid_at', 'method', 'amount', 'status', 'proof_path', 'notes'
+    ];
+
+    protected $casts = [
+        'paid_at' => 'datetime',
     ];
 
     public function invoice()
     {
         return $this->belongsTo(Invoice::class);
+    }
+
+    public function recordedBy()
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
