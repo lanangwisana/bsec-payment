51e12b1ec8504bb84570f294cc13fdac3fd005fc
lanangwisana <lanangwisana@gmail.com>
2026-02-11 17:47:12 +0800

    update Student Model

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'is_active', 'grade', 'school', 'program', 'parent_phone', 'parent_email'
+    ];
+
+    public function invoices()
+    {
+        return $this->hasMany(Invoice::class);
+    }
 }
