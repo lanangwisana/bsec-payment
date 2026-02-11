11fad07d07a8e4e860a7c2a6731ea705c5b5c964
lanangwisana <lanangwisana@gmail.com>
2026-02-11 17:48:22 +0800

    create invoice table

--- database/migrations/2026_02_09_071701_create_invoices_table.php
+++ database/migrations/2026_02_09_071701_create_invoices_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('student_id')->constrained()->onDelete('cascade');
+            $table->string('invoice_number')->unique();
+            $table->decimal('amount', 15, 2);
+            $table->string('month_name');
+            $table->integer('year');
+            $table->enum('status', ['pending', 'paid', 'unpaid'])->default('pending');
+            $table->date('due_date');
             $table->timestamps();
         });
     }
