7d6a5b5f1193afcd91609b0443ddb61bd0e65b02
lanangwisana <lanangwisana@gmail.com>
2026-02-11 17:46:50 +0800

    update Payment Model

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    protected $fillable = [
+        'invoice_id', 'paid_at', 'method', 'amount', 'status', 'proof_path', 'notes'
+    ];
+
+    public function invoice()
+    {
+        return $this->belongsTo(Invoice::class);
+    }
 }
