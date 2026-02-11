29eb19c22281f0db23a7150a25b358a8c361e453
lanangwisana <lanangwisana@gmail.com>
2026-02-11 17:46:33 +0800

    update Invoice Model

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Invoice extends Model
 {
-    //
+    protected $fillable = [
+        'student_id', 'invoice_number', 'amount', 'month_name', 'year', 'status', 'due_date'
+    ];
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class);
+    }
 }
